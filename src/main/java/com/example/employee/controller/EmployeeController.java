4f8c57e29223af8091ae065968feac31a03583d4
Mark Dsouza <markdsouza93@gmail.com>
2021-03-25 22:05:54 +0530

    Requestfix

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,24 +1,14 @@
 package com.example.employee.controller;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employee.model.EmployeeDetails;
 import com.example.employee.service.EmployeeService;
-import com.example.employee.service.EmployeeServiceImp;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class EmployeeController {
 	
 	
@@ -29,10 +19,10 @@ public class EmployeeController {
 		
 		this.empservice = empservice;
 	}
-	
+
+
 	@PostMapping("/employee/new")
-	
-	public String saveDetails(EmployeeDetails empdetails)
+	public String saveDetails( @RequestBody EmployeeDetails empdetails)
 	{
 		EmployeeDetails savedemp= empservice.save(empdetails);
 		
